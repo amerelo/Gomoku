32812371110034c348baf3384fb9d2418a9ea89c
Alexis Merelo <amerelo@student.42.fr>
2018-05-04 11:42:07 +0200

    add solver_iterative need alpha-beta purning

--- src/minmax/action.rs
+++ src/minmax/action.rs
@@ -6,9 +6,12 @@ pub struct Action
 	pub value: i32,
 	pub alpha: i32,
 	pub beta: i32,
-	pub map: Map,
-	pub x_y: (usize, usize),
+	pub depth: i32,
 	pub number_captured: usize,
+	pub x_y: (usize, usize),
+	pub evaluate: bool,
+	pub map: Map,
+	pub action_done: Vec<Action>,
 }
 
 impl Action
@@ -22,6 +25,24 @@ impl Action
 			map: map,
 			x_y: x_y,
 			number_captured: 0,
+			evaluate: false,
+			depth: 0,
+			action_done: vec![],
+		}
+	}
+
+	pub fn new_iterative(map: Map, x_y: (usize, usize), alpha_beta: (i32, i32), depth: i32) -> Self
+	{
+		Action {
+			value: 0,
+			alpha: alpha_beta.0,
+			beta: alpha_beta.1,
+			map: map,
+			x_y: x_y,
+			number_captured: 0,
+			evaluate: false,
+			depth: depth,
+			action_done: vec![],
 		}
 	}
 }
--- src/minmax/recursive.rs
+++ src/minmax/recursive.rs
@@ -36,6 +36,22 @@ fn place(map: Map, x: usize, y: usize, alpha_beta: (i32, i32)) -> Action
 	action
 }
 
+fn place_iterative(map: Map, x: usize, y: usize, alpha_beta: (i32, i32), depth: i32) -> Action
+{
+	let mut action = Action::new_iterative(map, (x, y), (alpha_beta.0, alpha_beta.1), depth);
+
+	// let slot_player = &find_slot_player![action.map.current_player, Slot::PlayerOne, Slot::PlayerTwo];
+	// let slot_enemy = &find_slot_enemy![action.map.current_player, Slot::PlayerOne, Slot::PlayerTwo];
+
+	// action.map.is_winning_move(x, y);
+
+	action.map.set_value((x as i64, y as i64), find_slot_player!(action.map.current_player));
+	// action.map.number_captured((x as i32, y as i32), find_slots_players![action.map.current_player], true);
+	action.map.change_player_turn();
+
+	action
+}
+
 fn best_action(turn: &Turn, new_action: Action, tmp: &mut Action, action_set: &mut bool)
 {
 	match *turn {
@@ -58,6 +74,173 @@ fn best_action(turn: &Turn, new_action: Action, tmp: &mut Action, action_set: &m
 	}
 }
 
+fn select_best_aciton(action_1: &mut Action, action_2: Action, turn: &Turn)
+{
+	match *turn {
+		Turn::MIN => {
+			if action_1.value > action_2.value{
+				*action_1 = action_2;
+			}
+		},
+		Turn::MAX => {
+			if action_1.value < action_2.value{
+				*action_1 = action_2;
+			}
+		},
+	}
+}
+
+#[allow(dead_code)]
+fn solver_iterative(depth: i32, map: &mut Map, turn: Turn, alpha_beta: (i32, i32)) -> Option<Action>
+{
+	let mut go_stack: Vec<Action> = vec![];
+
+	// let mut action_set = false;
+	let mut current_elem: Action = Action::new_iterative(map.clone(), (0, 0), (alpha_beta.0, alpha_beta.1), depth);
+	let mut new_action: Option<Action> = None;
+	
+	let mut dep;
+	let alpha = alpha_beta.0;
+	let beta = alpha_beta.1;
+	let mut new_trun: Turn = Turn::MAX;
+
+	'start_of_loop: loop 
+	{
+		if current_elem.depth == 0
+		{
+									// last_action.value =	heuristic::map_value(map, (&Slot::PlayerTwo, &Slot::PlayerOne));
+			current_elem.value = 0;
+			current_elem.evaluate = true;
+			match go_stack.pop()
+			{
+				Some(mut compare_action) => {
+					if compare_action.evaluate == false
+					{
+						compare_action.action_done.push(current_elem);
+						current_elem = compare_action; 
+						new_trun = change_turn(&new_trun);
+					} 
+					else if current_elem.depth != compare_action.depth
+					{
+						//check if current_elem.depth < compare_action.depth
+						// alpha beta 
+						compare_action.value = current_elem.value;
+						if compare_action.depth != depth
+						{
+							current_elem = compare_action; 
+						}
+						new_trun = change_turn(&new_trun);
+					}
+					else
+					{
+						select_best_aciton(&mut current_elem, compare_action, &turn);
+					}
+				} 
+				_ => break 'start_of_loop,
+			};
+
+		}
+		else if current_elem.evaluate == false
+		{
+			let area = map.area_of_interest();
+			current_elem.evaluate = true;
+			let new_map = current_elem.map.clone();
+			let a = alpha;
+			let b = beta;
+			dep = current_elem.depth -1; // test if not move
+
+			go_stack.push(current_elem.clone());
+			'root: for y_x in area.iter()
+			{
+				if map.is_available((y_x.1 , y_x.0)) == 0
+				{
+					match new_action {
+						Some(action) => go_stack.push(action),
+						_  => (),
+					};
+					new_action = Some(place_iterative(new_map.clone(), y_x.1 as usize , y_x.0 as usize, (a, b), dep));
+					
+				}
+			}
+
+			match new_action {
+				Some(tmp_a)		=> { current_elem = tmp_a },
+				_				=> {
+					match go_stack.pop()
+					{
+						Some(action) => current_elem = action,
+						_ 			=> break 'start_of_loop,
+					}
+				},
+			};
+
+			new_action = None;
+			new_trun = change_turn(&new_trun);
+		}
+		else if current_elem.evaluate == true
+		{
+			if !current_elem.action_done.is_empty()
+			{
+				while let Some(tmp_action) = current_elem.action_done.pop()
+				{
+					select_best_aciton(&mut current_elem, tmp_action, &turn);
+				}
+			}
+
+			match go_stack.pop()
+			{
+				Some(mut compare_action) => {
+					if compare_action.evaluate == false
+					{
+						compare_action.action_done.push(current_elem);
+						current_elem = compare_action; 
+						new_trun = change_turn(&new_trun);
+					} 
+					else if current_elem.depth != compare_action.depth
+					{
+						//check if current_elem.depth < compare_action.depth
+						// alpha beta 
+						compare_action.value = current_elem.value;
+						if compare_action.depth != depth
+						{
+							current_elem = compare_action; 
+						}
+						new_trun = change_turn(&new_trun);
+					}
+					else
+					{
+						select_best_aciton(&mut current_elem, compare_action, &turn);
+					}
+				} 
+				_ => break 'start_of_loop,
+			};
+
+			// let mut compare_action = go_stack.pop().unwrap();
+			// 											// first slot is for the player we want the score
+			// last_action.value =	heuristic::map_value(map, (&Slot::PlayerTwo, &Slot::PlayerOne));
+			// current_elem.value = 0;
+			// current_elem.evaluate = true;
+
+			// match
+			// {
+				// Some(action) => {
+				// new_action.value = action.value;
+				// best_action(&turn, new_action, &mut tmp, &mut action_set);
+				// },
+				// None => (),
+			// }
+
+			// if tmp.alpha >= tmp.beta
+			// {
+			// 	break 'root;
+			// }
+		}
+	}
+
+	return Some(current_elem);
+}
+
+#[allow(dead_code)]
 fn solver(depth: i32, map: &mut Map, turn: Turn, alpha_beta: (i32, i32)) -> Option<Action>
 {
 	if depth == 0
@@ -110,7 +293,8 @@ pub fn start_min_max(map: &Map) -> Option<Action>
 {
 	let depth: i32 = 3;
 
-	let action = solver(depth, &mut map.clone(), Turn::MAX, (MIN, MAX));
+	// let action = solver(depth, &mut map.clone(), Turn::MAX, (MIN, MAX));
+	let action = solver_iterative(depth, &mut map.clone(), Turn::MAX, (MIN, MAX));
 	// let action = None;
 
 	return action;
